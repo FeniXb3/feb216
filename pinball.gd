e6ea1f8e855474981231f3073e5f4a4f80c3f052
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-14 16:43:03 +0200

    Restart after 1 second

--- pinball.gd
+++ pinball.gd
@@ -1,6 +1,10 @@
 extends Node2D
-
+@export var restart_timer: Timer
 
 func _on_killzone_body_entered(_body: Node2D) -> void:
-	get_tree().reload_current_scene.call_deferred()
 	print("Game over")
+	restart_timer.start()
+
+
+func _on_restart_timer_timeout() -> void:
+	get_tree().reload_current_scene.call_deferred()
