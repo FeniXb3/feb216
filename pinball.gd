afcbffce1b7a5f3213f2de65433eb20118733f22
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-14 17:21:44 +0200

    Add swirling paddles

--- pinball.gd
+++ pinball.gd
@@ -1,5 +1,14 @@
 extends Node2D
 @export var restart_timer: Timer
+@export var left_paddle: RigidBody2D
+@export var right_paddle: RigidBody2D
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_left"):
+		left_paddle.apply_torque_impulse(-5000)
+		
+	if event.is_action_pressed("ui_right"):
+		right_paddle.apply_torque_impulse(5000)
 
 func _on_killzone_body_entered(_body: Node2D) -> void:
 	print("Game over")
